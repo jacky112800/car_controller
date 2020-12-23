eebd3899dd1818ffa539f864f9b6ca7916edc2f1
jacky112800 <cat2cat6@gmail.com>
2020-12-23 23:20:59 +0800

    傳輸處理試做中途

--- car/app/src/main/java/com/example/car/camera.kt
+++ car/app/src/main/java/com/example/car/camera.kt
@@ -1,12 +1,11 @@
 package com.example.car
 
+import android.app.Service
 import android.content.Intent
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.Color
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
+import android.os.*
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,8 +13,8 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import io.github.controlwear.virtual.joystick.android.JoystickView
 import kotlinx.android.synthetic.main.activity_camera.*
-import java.io.File
-import java.io.InputStream
+import android.os.Vibrator
+
 
 @RequiresApi(Build.VERSION_CODES.O)
 
@@ -23,46 +22,51 @@ class camera : AppCompatActivity() {
 
     private var m_angle_tv: TextView? = null
     private var m_strength_tv: TextView? = null
-    var img_byte:ByteArray? = null
-    val img_view_car=null
-
+    var img_byte: ByteArray? = null
+    val img_view_car = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-
         //joystick
         m_angle_tv = findViewById<View>(R.id.angle_tv) as TextView
         m_strength_tv = findViewById<View>(R.id.strength_tv) as TextView
-        val joystick =findViewById<JoystickView>(R.id.joystickView_car)
+        val joystick = findViewById<JoystickView>(R.id.joystickView_car)
         joystick.setOnMoveListener { angle, strength ->
             m_angle_tv!!.setText(angle.toString())
             m_strength_tv!!.setText(strength.toString())
         }
-    right_left_btn.setOnClickListener(object : View.OnClickListener {
-        override fun onClick(v: View?) {
+        right_left_btn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
 //            right_left_btn.setBackgroundResource(R.drawable.round_btn_change_color)
-            draw_img()
+                draw_img()
+                doVibrate()
 
+            }
 
+        })
+        //joystick
+    }
+
+    fun doVibrate() {
+        var button_vibrator = application.getSystemService(Service.VIBRATOR_SERVICE) as Vibrator
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            button_vibrator.vibrate(
+                VibrationEffect.createOneShot(
+                    200,
+                    VibrationEffect.DEFAULT_AMPLITUDE
+                )
+            )
+        } else {
+            button_vibrator.vibrate(100)
         }
 
-    })
-        //joystick
-
-
-
-
     }
 
-    fun readbyte (view:View){
-//        val inputStream:InputStream=File("886_886b").inputStream()
-
-    }
-
-    fun to_setting(view: View){
-        val intent_setting=Intent(this, setting::class.java)
+    fun to_setting(view: View) {
+        val intent_setting = Intent(this, setting::class.java)
         startActivity(intent_setting)
     }
 
@@ -71,21 +75,22 @@ class camera : AppCompatActivity() {
 //        button.setBackgroundResource(R.drawable.round_btn_change_color)
 //    }
 
-    fun draw_img(){
-        val img_view_car=findViewById<ImageView>(R.id.img_view_car_to_iphone)
+    fun draw_img() {
+        val img_view_car = findViewById<ImageView>(R.id.img_view_car_to_iphone)
 
         val w: Int = 886
         val h: Int = 886
         val compare = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        var r: Int=(0..255).random()
-        var g: Int=(0..255).random()
-        var b: Int=(0..255).random()
+        var r: Int = (0..255).random()
+        var g: Int = (0..255).random()
+        var b: Int = (0..255).random()
+//        var get_pixel_data=client_th().get_data
 
-        var ccc:Int=0xff000000.toInt()
+
 
         for (x in 0 until w) {
             for (y in 0 until h) {
-                compare.setPixel(x,y,Color.rgb(0xff,0xff,0xff))
+                compare.setPixel(x, y, Color.rgb(0xff, 0xff, 0xff))
             }
         }
         img_view_car.setImageBitmap(compare)
---
+++ car/app/src/main/java/com/example/car/client_th.kt
@@ -0,0 +1,55 @@
+package com.example.car
+
+import java.io.DataInputStream
+import java.io.OutputStream
+import java.net.Socket
+import java.nio.charset.Charset
+import java.util.*
+import kotlin.concurrent.thread
+
+class client_th : Thread() {
+    var get_data:ByteArray= byteArrayOf()
+    override fun run() {
+        val address = "172.25.128.1"
+        val port = 5050
+        val client = client_controller(address, port)
+        client.run()
+        var get_data: ByteArray = client.img_bt
+    }
+}
+
+
+class client_controller(address: String, port: Int) {
+    private val connection: Socket = Socket(address, port)
+    private var connected: Boolean = true
+    var data_in: DataInputStream = DataInputStream(connection.getInputStream())
+
+    init {
+        println("Connected to server at $address on port $port")
+    }
+
+    private val reader: Scanner = Scanner(connection.getInputStream())
+    private val writer: OutputStream = connection.getOutputStream()
+    var img_bt = byteArrayOf()
+    fun run() {
+        val length: Int = data_in.readInt()
+        var img_bt = ByteArray(2764816)
+        data_in.readFully(img_bt, 0, img_bt.size)
+
+        reader.close()
+        connection.close()
+    }
+
+    private fun write(message: String) {
+        writer.write((message + '\n').toByteArray(Charset.defaultCharset()))
+    }
+
+    private fun read() {
+        while (connected)
+            println(reader.nextLine())
+    }
+}
+
+
+
+
